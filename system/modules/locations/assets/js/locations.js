23f62396c83d2a6c1b9896398cd87bb8642ea451
stjeanmark <52003247+stjeanmark@users.noreply.github.com>
2019-09-12 09:16:07 -0400

    Update locations.js

--- system/modules/locations/assets/js/locations.js
+++ system/modules/locations/assets/js/locations.js
@@ -3,72 +3,85 @@
 (function($) {
 $(document).ready(function() {
 	
+
+		//hide all divs by default
+		$(".location_full").each(function(){
+			$(this).hide();
+		});
+	
 	//when making a change to the category selector
 	$("select.locations_category_selector").change(function() {
-		//automatically disable it every time its changed
-		//$("input.locations_zip_input").prop('disabled', true);
-		
-		//not sure what this section does, I will find out
-		$('select.locations_state_selector').prop('selectedIndex',0);
-		$('div.mod_locations_list').each(function(i, cte) {
-				var module = $(this);
-				var selector = module.find('select.locations_state_selector');
-				module.find('div.state').hide();
-			  });
-			  
+		//hide all divs again
+		$(".location_full").each(function(){
+			$(this).hide();
+		});
+	
 		//if a category is selected, enabled ZIP input
-		if ($(this).val() != '') {
+		if ($(this).val() !== '') {
 			$("input.locations_zip_input").prop('disabled', false);
+			
+			
 		}
 		//if no category selected, keep ZIP disabled	
 		else {
 			$("input.locations_zip_input").prop('disabled', true);
-		
-			//need to investigate more
-			  $('div.mod_locations_list').each(function(i, cte) {
-				var module = $(this);
-				var selector = module.find('select.locations_state_selector');
-				module.find('div.state').hide();
-			  });
-		}
-			
+		}	
 	});
 	
 	
 	//when changing zip
 	$("input.locations_zip_input").keyup(function(){
-	
-		// go through each listing and check if category AND zip match
-		alert(this.value);
+		//if the zip is the right characters in length or longer
+		if($(this).val().length >=5 )
+		{
+		//get the category selector's value
+		var catVal = $("select.locations_category_selector").val();
+		var zipVal = $("input.locations_zip_input").val();
+		
+		var cat_found = 0;
+		var zip_found = 0;
+		var counter = 0;
+		
+		//loop through each listing, and their children divs
+		$('.location_full').find('div').each(function(){
+			counter = counter + 1;
+			var innerDivId = $(this).attr('class');	
+			var innerDivText = $(this).html();
+			if(innerDivId == "hidden_cat_id")
+			{
+					if(innerDivText.includes(catVal))
+					{
+						// something
+						cat_found = 1;
+					}
+			}
+			if(innerDivId == "hidden_zip")
+			{
+				if(innerDivText.includes(zipVal))
+				{
+					zip_found = 1;
+					//alert("found_2");
+				}
+			}
+			if(counter == 4)
+			{
+				if(cat_found == 1 && zip_found == 1)
+				{
+					//make parent div visible
+					$(this).parent().show();
+					alert($(this).parent().html());
+				}
+				counter = 0;
+				cat_found = 0;
+				zip_found = 0;
+			}
+
+		});
+		}
+		
+		//alert(catVal);
 	});
 
-	
-	//this is the big one, showing or now showing locations
-	
-	
-	/*
-  $('div.mod_locations_list').each(function(i, cte) {
-	var module = $(this);
-	var selector = module.find('select.locations_state_selector');
-	var catSelector = module.find('select.locations_category_selector');
-	module.find('div.state').hide();
-	selector.change(function() {
-		module.find('div.state').hide();		
-		state = selector.find('option:selected').val();
-		cat = catSelector.find('option:selected').val();
-		module.find('div.location_full').hide();
-		var catL = 'div.cat_' + cat;
-		module.find(catL).show();
-		var showEl = module.find('div.state_' + state);
-		if (showEl.length > 0) {
-			showEl.show();
-		} else {
-			module.find('div.state_not_found').show();
-		}
-	});
-  });
-  $('div.mod_locations_list select.locations_state_selector').first().change();
-  */
+
 });
 })(jQuery);
-
