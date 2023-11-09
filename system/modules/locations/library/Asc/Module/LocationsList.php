e5edb9753f58f5242168d85b90d655c01dd37ea2
Mark St. Jean <52003247+stjeanmark@users.noreply.github.com>
2023-11-09 13:54:36 -0500

    Update LocationsList.php

--- system/modules/locations/library/Asc/Module/LocationsList.php
+++ system/modules/locations/library/Asc/Module/LocationsList.php
@@ -36,7 +36,7 @@ class LocationsList extends \Contao\Module
 	public function __construct($objModule, $strColumn='main')
 	{
 		parent::__construct($objModule, $strColumn);
-		$this->arrStates = Locations::getStates();
+		$this->arrStates = $this->getStates();
 	}
 	
     /**
@@ -167,5 +167,73 @@ class LocationsList extends \Contao\Module
 		}
 		return ($a['Name'] < $b['Name']) ? -1 : 1;
 	}
+	
+	public function getStates()
+    {		
+        return array(
+			"United States" => array(
+				'AL' => 'Alabama',
+				'AK' => 'Alaska',
+				'AZ' => 'Arizona',
+				'AR' => 'Arkansas',
+				'CA' => 'California',
+				'CO' => 'Colorado',
+				'CT' => 'Connecticut',
+				'DE' => 'Delaware',
+				'FL' => 'Florida',
+				'GA' => 'Georgia',
+				'HI' => 'Hawaii',
+				'ID' => 'Idaho',
+				'IL' => 'Illinois',
+				'IN' => 'Indiana',
+				'IA' => 'Iowa',
+				'KS' => 'Kansas',
+				'KY' => 'Kentucky',
+				'LA' => 'Louisiana',
+				'ME' => 'Maine',
+				'MD' => 'Maryland',
+				'MA' => 'Massachusetts',
+				'MI' => 'Michigan',
+				'MN' => 'Minnesota',
+				'MS' => 'Mississippi',
+				'MO' => 'Missouri',
+				'MT' => 'Montana',
+				'NE' => 'Nebraska',
+				'NV' => 'Nevada',
+				'NH' => 'New Hampshire',
+				'NJ' => 'New Jersey',
+				'NM' => 'New Mexico',
+				'NY' => 'New York',
+				'NC' => 'North Carolina',
+				'ND' => 'North Dakota',
+				'OH' => 'Ohio',
+				'OK' => 'Oklahoma',
+				'OR' => 'Oregon',
+				'PA' => 'Pennsylvania',
+				'RI' => 'Rhode Island',
+				'SC' => 'South Carolina',
+				'SD' => 'South Dakota',
+				'TN' => 'Tennessee',
+				'TX' => 'Texas',
+				'UT' => 'Utah',
+				'VT' => 'Vermont',
+				'VA' => 'Virginia',
+				'WA' => 'Washington',
+				'WV' => 'West Virginia',
+				'WI' => 'Wisconsin',
+				'WY' => 'Wyoming',
+				'AS' => 'American Samoa',
+				'DC' => 'District of Columbia',
+				'FM' => 'Federated States of Micronesia',
+				'GU' => 'Guam',
+				'MH' => 'Marshall Islands',
+				'MP' => 'Northern Mariana Islands',
+				'PW' => 'Palau',
+				'PR' => 'Puerto Rico',
+				'VI' => 'Virgin Islands'),
+			"Canada" => array(
+				"CAN" => "All Provinces")
+			);
+    }
 
 } 
